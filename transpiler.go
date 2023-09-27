d2d02818affec55706b5e056dcd77d894f3d385e
Luan <luan.carlos.3174@gmail.com>
2023-09-27 20:45:48 -0300

    feat: adds check to not replace string literals

--- transpiler.go
+++ transpiler.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -12,6 +13,8 @@ type Token struct {
 	Btt string `json:"btt"`
 	Py  string `json:"py"`
 }
+
+const placeholder = "${LITERAL_PLACEHOLDER}$"
 
 func Transpile(code string) string {
 	file, err := os.Open("tokens.json")
@@ -32,8 +35,34 @@ func Transpile(code string) string {
 		log.Fatal("Error unmarshaling JSON:", err)
 	}
 
+	// Replace and remove the double-quoted strings from the original text
+	code, literals := RemoveStringLiterals(code)
+
 	for _, token := range tokens {
 		code = strings.ReplaceAll(code, token.Btt, token.Py)
+	}
+
+	// Return the double-quoted strings to the original text
+	code = ReturnStringLiterals(code, literals)
+
+	return code
+}
+
+func RemoveStringLiterals(code string) (string, []string) {
+	regex := regexp.MustCompile(`"[^"]*"`)
+	var literals []string
+
+	code = regex.ReplaceAllStringFunc(code, func(matched string) string {
+		literals = append(literals, matched)
+		return placeholder
+	})
+
+	return code, literals
+}
+
+func ReturnStringLiterals(code string, literals []string) string {
+	for _, str := range literals {
+		code = strings.Replace(code, placeholder, str, 1)
 	}
 
 	return code
